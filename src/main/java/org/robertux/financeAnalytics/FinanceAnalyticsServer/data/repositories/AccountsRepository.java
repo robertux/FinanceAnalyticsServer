9cd618df4586d7e9134383e62f662106cefbaac6
Roberto Linares <blendboy@gmail.com>
2018-07-31 00:10:04 -0600

    Adición preliminar de las clases AccountsRepository y TransactionsRepository

---
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/AccountsRepository.java
@@ -0,0 +1,7 @@
+package org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories;
+
+import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.tables.records.AccountRecord;
+
+public class AccountsRepository extends BaseRepository<AccountRecord> {
+
+}
---
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/TransactionsRepository.java
@@ -0,0 +1,7 @@
+package org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories;
+
+import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.tables.records.TransactionRecord;
+
+public class TransactionsRepository extends BaseRepository<TransactionRecord> {
+
+}
