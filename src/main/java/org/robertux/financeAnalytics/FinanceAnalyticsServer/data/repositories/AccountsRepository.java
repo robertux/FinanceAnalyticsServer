69f4980072f588c4e6d4262649b99d0793f6aacb
Roberto Linares <blendboy@gmail.com>
2018-08-01 22:26:16 -0600

    Implementación de métodos en los repositorios de cuentas y transacciones para obtener registros

--- src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/AccountsRepository.java
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/AccountsRepository.java
@@ -1,7 +1,34 @@
 package org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.message.MessageFormatMessage;
+import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.tables.Account;
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.tables.records.AccountRecord;
 
 public class AccountsRepository extends BaseRepository<AccountRecord> {
 
+	public List<AccountRecord> getAll(long usrId) {
+		List<AccountRecord> accounts = new ArrayList<>(0);
+		try (Connection cn = config.getConnection()) {
+			accounts.addAll(getContext(cn).selectFrom(Account.ACCOUNT).where(Account.ACCOUNT.USER_ID.eq(usrId)).orderBy(Account.ACCOUNT.ALIAS).fetch());
+
+		} catch (Exception ex) {
+			this.logger.error(new MessageFormatMessage("Excepción obteniendo la lista de cuentas para el usuario con ID {}", usrId), ex);
+		}
+		return accounts;
+	}
+	
+	public AccountRecord get(long accountNumber) {
+		AccountRecord acc = null;
+		try (Connection cn = config.getConnection()) {
+			acc = getContext(cn).selectFrom(Account.ACCOUNT).where(Account.ACCOUNT.NUMBER.eq(accountNumber)).fetchOne();
+
+		} catch (Exception ex) {
+			this.logger.error(new MessageFormatMessage("Excepción obteniendo cuenta con número {}", accountNumber), ex);
+		}
+		return acc;
+	}
 }
