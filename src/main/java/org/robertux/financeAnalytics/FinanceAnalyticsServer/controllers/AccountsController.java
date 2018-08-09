51c57e6bfb205fa0ed6d57746475951938e211e5
Roberto Linares <blendboy@gmail.com>
2018-08-09 13:41:18 -0600

    Adición de métodos REST para el manejo de cuentas

--- src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/controllers/AccountsController.java
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/controllers/AccountsController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.Account;
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +21,37 @@ public class AccountsController {
 	private AccountsRepository accRepo;
 
 	@GetMapping("/users/{userId}/accounts")
-	public List<Account> getAccounts(@PathVariable("userId") long userId) {
-		
-		return accRepo.findAllByUserId(userId);
+	public ResponseEntity<List<Account>> getAccounts(@PathVariable("userId") long userId) {
+		return ResponseEntity.ok(accRepo.findAllByUserId(userId));
+	}
+	
+	@PutMapping("/users/{userId}/accounts")
+	public ResponseEntity<?> addAccount(@RequestBody Account acc) {
+		if (accRepo.findById(acc.getNumber()).isPresent()) {
+			return ResponseEntity.badRequest().body("Ya existe una cuenta con este número");
+		} else {
+			Account newAcc = accRepo.save(acc);
+			return ResponseEntity.ok(newAcc);
+		}
+	}
+	
+	@PostMapping("/users/{userId}/accounts")
+	public ResponseEntity<?> editAccount(@RequestBody Account acc) {
+		if (accRepo.findById(acc.getNumber()).isPresent()) {
+			Account newAcc = accRepo.save(acc);
+			return ResponseEntity.ok(newAcc);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/users/{userId}/accounts")
+	public ResponseEntity<?> deleteAccount(@RequestBody Account acc) {
+		if (accRepo.findById(acc.getNumber()).isPresent()) {
+			accRepo.delete(acc);
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
