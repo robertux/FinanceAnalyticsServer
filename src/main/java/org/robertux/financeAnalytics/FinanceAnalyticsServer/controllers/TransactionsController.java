963b8ab43bfc37c37dd1792eba5c0c5157cd44c9
Roberto Linares <blendboy@gmail.com>
2018-08-11 19:34:27 -0600

    Adición de funcionalidad para paginación de resultados. Closes #18

--- src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/controllers/TransactionsController.java
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/controllers/TransactionsController.java
@@ -6,6 +6,8 @@ import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.Transa
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories.AccountsRepository;
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories.TransactionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users/{userId}/accounts/{accNumber}/transactions")
 public class TransactionsController {
+	public static final int DEFAULT_PAGE_SIZE = 20;
+	public static final PageRequest DEFAULT_PAGER = PageRequest.of(0, DEFAULT_PAGE_SIZE, Direction.DESC, "date");
 
 	@Autowired
 	private TransactionsRepository trnRepo;
@@ -26,19 +28,34 @@ public class TransactionsController {
 	@Autowired
 	private AccountsRepository accRepo;
 
-	@GetMapping
+	@GetMapping("/users/{userId}/accounts/{accNumber}/transactions")
 	public ResponseEntity<List<Transaction>> getTransactions(@PathVariable("accNumber") long accountNumber) {
-		return ResponseEntity.ok(trnRepo.findAllByAccountNum(accountNumber));
+		return ResponseEntity.ok(trnRepo.findAllByAccountNum(accountNumber, DEFAULT_PAGER));
 	}
 	
-	@PutMapping
+	@GetMapping("/users/{userId}/accounts/{accNumber}/transactions/page/{pageNum}")
+	public ResponseEntity<List<Transaction>> getTransactionsPaged(@PathVariable("accNumber") long accountNumber, @PathVariable("pageNum") int pageNum) {
+		return ResponseEntity.ok(trnRepo.findAllByAccountNum(accountNumber, PageRequest.of(pageNum, DEFAULT_PAGE_SIZE, Direction.DESC, "date")));
+	}
+	
+	@GetMapping("/users/{userId}/transactions")
+	public ResponseEntity<List<Transaction>> getAllTransactions(@PathVariable("userId") long userId) {
+		return ResponseEntity.ok(trnRepo.findAllByUser(userId, DEFAULT_PAGER));
+	}
+	
+	@GetMapping("/users/{userId}/transactions/page/{pageNum}")
+	public ResponseEntity<List<Transaction>> getAllTransactionsPaged(@PathVariable("userId") long userId, @PathVariable("pageNum") int pageNum) {
+		return ResponseEntity.ok(trnRepo.findAllByUser(userId, PageRequest.of(pageNum, DEFAULT_PAGE_SIZE, Direction.DESC, "date")));
+	}
+	
+	@PutMapping("/users/{userId}/accounts/{accNumber}/transactions")
 	public ResponseEntity<?> addTransaction(@RequestBody Transaction trn, @PathVariable("accNumber") long accountNumber) {
 		trn.setAccount(accRepo.findById(accountNumber).get());
 		Transaction newTrn = trnRepo.save(trn);
 		return ResponseEntity.ok(newTrn);
 	}
 	
-	@PostMapping
+	@PostMapping("/users/{userId}/accounts/{accNumber}/transactions")
 	public ResponseEntity<?> editTransaction(@RequestBody Transaction trn, @PathVariable("accNumber") long accountNumber) {
 		if (trnRepo.findById(trn.getId()).isPresent()) {
 			trn.setAccount(accRepo.findById(accountNumber).get());
@@ -49,7 +66,7 @@ public class TransactionsController {
 		}
 	}
 	
-	@DeleteMapping
+	@DeleteMapping("/users/{userId}/accounts/{accNumber}/transactions")
 	public ResponseEntity<?> deleteTransaction(@RequestBody Transaction trn) {
 		if (trnRepo.findById(trn.getId()).isPresent()) {
 			trnRepo.delete(trn);
--- src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/TransactionsRepository.java
+++ src/main/java/org/robertux/financeAnalytics/FinanceAnalyticsServer/data/repositories/TransactionsRepository.java
@@ -3,12 +3,16 @@ package org.robertux.financeAnalytics.FinanceAnalyticsServer.data.repositories;
 import java.util.List;
 
 import org.robertux.financeAnalytics.FinanceAnalyticsServer.data.entities.Transaction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface TransactionsRepository extends CrudRepository<Transaction, Long> {
 
-	@Query("SELECT t FROM Transaction t WHERE t.account.number = :accountNumber ORDER BY t.date DESC")
-	public List<Transaction> findAllByAccountNum(@Param("accountNumber") long accountNumber);
+	@Query("SELECT t FROM Transaction t WHERE t.account.number = :accountNumber")
+	public List<Transaction> findAllByAccountNum(@Param("accountNumber") long accountNumber, Pageable pg);
+	
+	@Query("SELECT t FROM Transaction t WHERE t.account.user.id = :userId")
+	public List<Transaction> findAllByUser(@Param("userId") long userId, Pageable pg);
 }
