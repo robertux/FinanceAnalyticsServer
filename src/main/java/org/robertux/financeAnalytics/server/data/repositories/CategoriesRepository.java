6cc74ff589a9814d1d0f86d5a609e533ef983621
Roberto Linares <blendboy@gmail.com>
2019-05-01 23:46:32 -0600

    Corrección del nombre de la clase CategoriesRepository

--- src/main/java/org/robertux/financeAnalytics/server/data/repositories/CategoriesRepository.java
+++ src/main/java/org/robertux/financeAnalytics/server/data/repositories/CategoriesRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UsersRepository extends CrudRepository<Category, String> {
+public interface CategoriesRepository extends CrudRepository<Category, String> {
 
 	@Query("SELECT c FROM Category c WHERE c.name = :name")
 	public Optional<Category> findByName(@Param("name") String name);
