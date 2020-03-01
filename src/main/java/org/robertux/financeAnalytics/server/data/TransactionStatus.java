2308dd27328ecf3257bdd7e27b83f61ec4cc04e9
Roberto Linares <blendboy@gmail.com>
2020-02-29 22:55:55 -0600

    Adición de nuevos posibles status a los beans de TransactionStatus y UserStatus

--- src/main/java/org/robertux/financeAnalytics/server/data/TransactionStatus.java
+++ src/main/java/org/robertux/financeAnalytics/server/data/TransactionStatus.java
@@ -8,6 +8,7 @@ public class TransactionStatus extends KeyValue {
 	@JsonIgnore public static final TransactionStatus REJECTED = new TransactionStatus("R", "Rejected");
 	@JsonIgnore public static final TransactionStatus FAILED = new TransactionStatus("F", "Failed");
 	@JsonIgnore public static final TransactionStatus PENDING = new TransactionStatus("P", "Pending");
+	@JsonIgnore public static final TransactionStatus CANCELLED = new TransactionStatus("C", "Cancelled");
 	
 	private TransactionStatus(String key, String value) {
 		super(key, value);
--- src/main/java/org/robertux/financeAnalytics/server/data/UserStatus.java
+++ src/main/java/org/robertux/financeAnalytics/server/data/UserStatus.java
@@ -6,6 +6,7 @@ public class UserStatus extends KeyValue {
 	
 	@JsonIgnore public static final UserStatus ACTIVE = new UserStatus("A", "Active");
 	@JsonIgnore public static final UserStatus INACTIVE = new UserStatus("I", "Inactive");
+	@JsonIgnore public static final UserStatus SUSPENDED = new UserStatus("S", "Suspended");
 	
 	private UserStatus(String key, String value) {
 		super(key, value);
