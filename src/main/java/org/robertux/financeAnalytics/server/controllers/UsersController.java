cc8289b567887b6e6a5c40fab7e15b5e20efc080
Roberto Linares <blendboy@gmail.com>
2020-03-01 22:53:51 -0600

    Corrección en los mappings de los métodos

--- src/main/java/org/robertux/financeAnalytics/server/controllers/UsersController.java
+++ src/main/java/org/robertux/financeAnalytics/server/controllers/UsersController.java
@@ -16,9 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UsersController {
 
 	@Autowired
