cacf05b557d2a037d23c8cd326b4f14e35fbe3f7
Roberto Linares <blendboy@gmail.com>
2020-03-01 22:52:57 -0600

    Eliminación de validación innecesaria

--- src/main/java/org/robertux/financeAnalytics/server/App.java
+++ src/main/java/org/robertux/financeAnalytics/server/App.java
@@ -1,18 +1,11 @@
 package org.robertux.financeAnalytics.server;
 
-import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) throws Exception {
-		if (args.length < 1) {
-			LogManager.getLogger(App.class).error("Faltan parámetros necesarios: 1-Ruta del archivo de configuración para Spring");
-			return;
-		}
-		System.setProperty("spring.config.location", args[0]);
-		
 		SpringApplication.run(App.class, args);
 	}
 }
